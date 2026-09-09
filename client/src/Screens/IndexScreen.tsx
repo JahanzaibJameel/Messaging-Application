2acddab1cf7c398e584489963eb9db308f9693a1
jahanzaibjameel <m.jahanzaibjameel@gmail.com>
2026-09-10 00:37:02 +0500

    screens: add IndexScreen component for web build entry point

---
+++ client/src/Screens/IndexScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexScreen = () => {
+  return (
+    <div className="container">
+      <h1>ChatApp</h1>
+      <p>This is the main chat application screen.</p>
+    </div>
+  );
+};
+
+export default IndexScreen;
