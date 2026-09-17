c803a909cdbb4861a0f5e861cf02882abde98dfd
jahanzaibjameel <m.jahanzaibjameel@gmail.com>
2026-09-18 00:17:29 +0500

    chore(test): exclude .kilo directory from Jest module resolution
    Add modulePathIgnorePatterns so files under .kilo are not picked up as test modules.

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@
 module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  modulePathIgnorePatterns: ["<rootDir>/[.]kilo/"],
   testMatch: ["**/__tests__/**/*.(ts|tsx|js)", "**/*.(test|spec).(ts|tsx|js)"],
   collectCoverageFrom: [
     "client/**/*.{ts,tsx}",
